01376f42a93c75aca557f7086e2c630bb8a53423
chammem sinda <99534039+chammem@users.noreply.github.com>
2025-04-15 02:47:12 +0100

    main

---
+++ backend/src/main/java/tn/esprit/rh/achat/util/WebConfig.java
@@ -0,0 +1,15 @@
+package tn.esprit.rh.achat.util;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "SpringMVC");
+    }
+}
